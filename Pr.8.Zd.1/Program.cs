ea4c11fa5d4a507dd4017102e33d5e2f9cdc03a8
USER <USER@DESKTOP-TRQG01D>
2022-10-15 10:29:26 +0300

    Добавьте файлы проекта.

---
+++ Pr.8.Zd.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение х=");
+            double x = double.Parse(Console.ReadLine());
+            double y;
+
+            if (x > 2)
+                y = (x * (Math.Cbrt(x-2)));
+            else
+            {
+                if (x < 2)
+                    y = 1 / (Math.Pow(Math.Pow(Math.E, x), 2)) * ((Math.Pow(Math.Cos(x), 2)) - (Math.Pow(Math.Sin(x), 2)));
+                else
+                    y = x * (2*Math.Sin(x) * Math.Cos(x));
+            }
+            Console.WriteLine("y=" + y);
+
+        }
+    }
+}
